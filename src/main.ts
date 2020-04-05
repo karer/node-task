034e8c316aa4b42176c4f588775bc9fe53710b97
Kacper Geisheimer <devkarer@gmail.com>
2020-04-05 21:46:05 +0200

    Use http port from config service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger: Logger = new Logger('App');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService: ConfigService = app.get(ConfigService);
+  const port: number = configService.get('port');
+
+  await app.listen(port, () => {
+    logger.log(`* Listening on ::${port}`);
+  });
 }
+
 bootstrap();
