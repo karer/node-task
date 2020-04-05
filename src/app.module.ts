36d1fd98307a0ccb5dcc49e313ad4642290dd712
Kacper Geisheimer <devkarer@gmail.com>
2020-04-05 21:56:45 +0200

    Integrate TypeORM with basic config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { configFactory } from './config/config.factory';
+import { DatabaseConfigService } from './services/database/database-config.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configFactory] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configFactory] }),
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConfigService }),
+  ],
 })
 export class AppModule {}
---
+++ src/services/database/database-config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DatabaseConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get('database.host'),
+      port: this.configService.get('database.port'),
+      username: this.configService.get('database.username'),
+      password: this.configService.get('database.password'),
+      database: this.configService.get('database.name'),
+      entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    };
+  }
+}
