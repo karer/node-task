63259ea195b2f02c64c2672c97839c0018384f64
Kacper Geisheimer <devkarer@gmail.com>
2020-04-06 22:55:22 +0200

    Add products in cart definition

--- src/api/cart/interfaces/cart.interface.ts
+++ src/api/cart/interfaces/cart.interface.ts
@@ -1,5 +1,7 @@
 import * as mongoose from 'mongoose';
+import { Product } from '../../product/interfaces/product.interface';
 
 export interface Cart extends mongoose.Document {
   userId: string;
+  products: Product[];
 }
--- src/api/cart/schemas/cart.schema.ts
+++ src/api/cart/schemas/cart.schema.ts
@@ -1,6 +1,8 @@
 import * as mongoose from 'mongoose';
 import { Cart } from '../interfaces/cart.interface';
+import { ProductSchema } from '../../product/schemas/product.schema';
 
 export const CartSchema = new mongoose.Schema<Cart>({
   userId: { type: String, required: true, index: true },
+  products: [ProductSchema],
 });
