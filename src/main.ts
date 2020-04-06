537eb7e41d3b04686f8673a766c0ad2eae60548f
Kacper Geisheimer <devkarer@gmail.com>
2020-04-06 23:15:13 +0200

    Add validation support

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger: Logger = new Logger('App');
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService: ConfigService = app.get(ConfigService);
   const port: number = configService.get('app.port');
