f8fedd08a7bb0440e258fd2c70dabeed1fd590b1
Kacper Geisheimer <devkarer@gmail.com>
2020-04-06 21:45:12 +0200

    Fix auth module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -4,10 +4,22 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), UserModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('app.key'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
   providers: [JwtStrategy, AuthService],
   exports: [PassportModule],
   controllers: [AuthController],
