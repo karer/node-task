5e4b662d86575c6a7b6659d1b0a592bdaca19beb
Kacper Geisheimer <devkarer@gmail.com>
2020-04-06 21:36:23 +0200

    Remove MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { configFactory } from './config/config.factory';
-import { DatabaseConfigService } from './services/database/database-config.service';
 import { AuthModule } from './api/auth/auth.module';
 import { UserModule } from './api/user/user.module';
+import { CartModule } from './api/cart/cart.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configFactory] }),
-    TypeOrmModule.forRootAsync({ useClass: DatabaseConfigService }),
     AuthModule,
     UserModule,
+    CartModule,
   ],
 })
 export class AppModule {}
