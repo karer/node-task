fbbc494f6594bff87f227a3192c4e66ebf99d9a9
Kacper Geisheimer <devkarer@gmail.com>
2020-04-07 15:20:08 +0200

    Add currency validator

---
+++ src/services/currency/currency.validator.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { CurrencyService } from './currency.service';
+
+@ValidatorConstraint({ name: 'isManagedCurrency', async: true })
+@Injectable()
+export class IsManagedCurrenccyConstraint
+  implements ValidatorConstraintInterface {
+  constructor(private readonly currencyService: CurrencyService) {}
+
+  validate(value: string): boolean {
+    return Object.keys(this.currencyService.getAll()).includes(value);
+  }
+
+  defaultMessage(): string {
+    return 'Currency is not valid.';
+  }
+}
+
+// tslint:disable-next-line: typedef
+export const IsManagedCurrency = (validationOptions?: ValidationOptions) => (
+  object: object,
+  propertyName: string,
+): void => {
+  registerDecorator({
+    target: object.constructor,
+    propertyName,
+    validator: IsManagedCurrenccyConstraint,
+    options: validationOptions,
+  });
+};
