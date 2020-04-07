4f8a6d8231f6d1172737963731f2de8a7cd2704c
Kacper Geisheimer <devkarer@gmail.com>
2020-04-07 14:57:18 +0200

    Remove order module

--- src/api/order/order.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@UseGuards(AuthGuard())
-@Controller('order')
-export class OrderController {}
